1ecbd9eb9a15dbabe914be242419b56d39cda189
SaiKiranRam <55264888+SaiKiranRam@users.noreply.github.com>
2021-03-09 16:11:58 +0530

    Deleted_Old_Main_Branch_Code

--- SpringBootLearn1/src/main/java/edu/javaBrains/practice/entity/ProgrammerDAO.java
+++
@@ -1,7 +0,0 @@
-package edu.javaBrains.practice.entity;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProgrammerDAO extends CrudRepository<Programmer, Integer> {
-
-}
